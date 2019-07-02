fd5adcf06fea4715e75146bcfa7e463593eb9d85
Brian Porter <beporter@users.sourceforge.net>
2019-07-02 11:39:23 -0500

    Set up Circle test summary.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,7 +2,7 @@
 
  PATH=$(npm bin):$PATH
 
- npm run lint && JEST_JUNIT_OUTPUT="build/junit.xml" jest \
+ npm run lint && JEST_JUNIT_OUTPUT="build/test_results/jest/junit.xml" jest \
   --ci \
   --runInBand \
   --reporters=default \
