65128cb6ef7f0848fe754e228c2970cacc2f7c9d
Brian Porter <beporter@users.sourceforge.net>
2019-07-09 18:39:06 +0000

    Update Square to use a provided onClick event.

--- spec/Square.spec.tsx
+++ spec/Square.spec.tsx
@@ -5,9 +5,21 @@ import { Square } from '../src/Square';
 describe('<Square />', () => {
   test('outputs the provided value', () => {
     const mark = '🐶';
-    const wrapper = shallow(<Square value={mark} />);
+    const wrapper = shallow(<Square value={mark} clickHandler={() => undefined}/>);
 
     expect(wrapper.find('div').hasClass('square')).toBe(true);
     expect(wrapper.text()).toBe(mark);
+  });
+
+  test('makes click handlers callable', () => {
+    const spy = jest.fn((args) => args);
+    const mark = '🐱';
+    const wrapper = shallow(<Square value={mark} clickHandler={spy}/>);
+    const button = wrapper.find('button').first();
+    button.simulate('click');
+
+    const handler = button.prop('onClick');
+    expect(handler).toBeDefined();
+    expect(spy).toHaveBeenCalled();
   });
 });
