03b27244c52437325274b811ad28c3fb62fe4d43
Brian Porter <beporter@users.sourceforge.net>
2019-07-10 09:28:57 -0500

    Improve code style for .map(...) call.

--- src/Board.tsx
+++ src/Board.tsx
@@ -11,9 +11,11 @@ export const Board = (props: Props): JSX.Element => {
     return <Square value={mark} clickHandler={(event) => props.clickHandler(pos)} />;
   };
 
-  const board = Array.from(Array(9).keys()).map(
-    (i: number) => <div className="octothorp" key={i}>{renderSquare(props.squares[i], i)}</div>
-  );
+  const board = Array.from(Array(9).keys()).map((i: number) => {
+    return (
+      <div className="octothorp" key={i}>{renderSquare(props.squares[i], i)}</div>
+    );
+  });
 
   return (
     <div className="board">{board}</div>
