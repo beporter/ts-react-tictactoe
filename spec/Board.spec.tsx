421ce786ae589adf05db910151a7ddc3d7935f82
Brian Porter <beporter@users.sourceforge.net>
2019-07-12 12:23:10 -0500

    fixup board readability and tests

--- spec/Board.spec.tsx
+++ spec/Board.spec.tsx
@@ -1,31 +1,38 @@
 import { mount, shallow } from 'enzyme';
 import React from 'react';
 import { Board } from '../src/Board';
+import * as GameContext from '../src/GameContext';
 
 describe('<Board />', () => {
   test('has nine squares', () => {
-    const squares = [ // Contents don't matter cause `shallow()`.
-      'X', '', '',
-      '', '', '',
-      '', '', ''
-    ];
-    const wrapper = shallow(<Board squares={squares} clickHandler={() => undefined} />);
+    jest.spyOn(React, 'useContext')
+      .mockReturnValueOnce({xToPlay: true, board: ['O', '', '',
+        '', '', '',
+        '', '', ''
+      ]})
+      .mockReturnValueOnce(jest.fn());
+
+    const squares = Array(9).fill(''); // Contents don't matter cause `shallow()`.
+    const wrapper = shallow(<Board squares={squares} />);
 
     expect(wrapper.find('Square')).toHaveLength(9);
   });
 
   test('injects click handler into <Square>s', () => {
-    const spy = jest.fn((args) => args);
+    const spy = jest.spyOn(GameContext, 'useGameDispatch');
     const squares = [
-      'X', '', '',
+      'O', '', '',
       '', '', '',
       '', '', ''
     ];
-    const wrapper = mount(<Board squares={squares} clickHandler={spy} />);
-    wrapper.find('button').first().simulate('click');
+    const wrapper = mount(<GameContext.GameProvider><Board squares={squares} /></GameContext.GameProvider>);
 
-    const handler = wrapper.find('Square').first().prop('clickHandler');
-    expect(handler).toBeDefined();
+    wrapper.find('button').first().simulate('click'); // No-op
+    wrapper.find('button').at(2).simulate('click'); // Place an `X` top-middle.
+    wrapper.render();
+
+    expect(wrapper.find('Square').first().text()).toEqual('O');
+    expect(wrapper.find('Square').at(2).text()).toEqual('X');
     expect(spy).toHaveBeenCalled();
   });
 });
--- src/Board.tsx
+++ src/Board.tsx
@@ -8,10 +8,16 @@ type Props = {
 
 export const Board = (props: Props): JSX.Element => {
   const dispatch = useGameDispatch();
-  const board = props.squares.map((value: string, index: number, array: string[]) => {
+
+  const board = props.squares.map((value: string, index: number) => {
+    const callback = (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (value === '') {
+        return dispatch({type: 'move', position: index});
+      }
+    };
     return (
       <div className="octothorp" key={index}>
-        <Square value={value} clickHandler={(event) => value === '' ? dispatch({type: 'move', position: index}) : () => undefined} />
+        <Square value={value} clickHandler={callback} />
       </div>
     );
   });
